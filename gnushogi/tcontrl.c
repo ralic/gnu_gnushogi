5af36f1b81423a337214d22d2062f186792065eb
Yann Dirson <ydirson@free.fr>
2014-07-29 23:11:02 +0200

    Move TCmove, TCminutes etc to tcontrl.h.

--- gnushogi/tcontrl.c
+++ gnushogi/tcontrl.c
@@ -43,6 +43,9 @@ int timeopp[MINGAMEIN], timecomp[MINGAMEIN];
 int compptr, oppptr;
 long time0 = 0, et = 0;
 struct TimeControlRec TimeControl;
+int   TCadd = 0;
+short TCmoves, TCminutes, TCseconds, OperatorTime;
+bool  TCflag;
 
 /*
  * In a networked enviroment gnushogi might be compiled on different hosts
